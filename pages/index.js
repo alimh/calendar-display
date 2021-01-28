b2015e67f0695b2e579563a9a5debb4f049c6eed
Alim Haji <alim.haji@gmail.com>
2021-01-27 21:19:26 -0700

    Read calendars with google service account

--- pages/index.js
+++ pages/index.js
@@ -1,171 +1,89 @@
 import Head from 'next/head';
 import { useEffect, useLayoutEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
+import {google} from 'googleapis';
 
 export default function Home(props) {
-  const [signedInState, setSignedInState] = useState(false);
+//       /**
+//        * Append a pre element to the body containing the given message
+//        * as its text node. Used to display the results of the API call.
+//        *
+//        * @param {string} message Text to be placed in pre element.
+//        */
+//       function appendPre(message) {
+//         var ul = document.getElementById('event-list');
+//         var li = document.createElement('li');
+//         var textContent = document.createTextNode(message + '\n');
+//         li.appendChild(textContent);
+//         ul.appendChild(li);
+//       }
 
-  const CLIENT_ID = props.client_id;
-  const API_KEY = props.api_key;
+//       /**
+//        * Print the summary and start datetime/date of the next ten events in
+//        * the authorized user's calendar. If no events are found an
+//        * appropriate message is printed.
+//        */
+//       async function listUpcomingEvents() {
+//         var now = new Date();
+//         var today = new Date(now.toDateString());
+//         var tomorrow = new Date(today.getTime() + 86400000);
 
-  const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+//         editHeader(now.toDateString().concat('...'));
+//         // get all the calendar
 
-  // Authorization scopes required by the API; multiple scopes can be
-  // included, separated by spaces.
-  const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+//           var calFetches = [];
+//           calIds.forEach(function(c) {
+//             calFetches.push(gapi.client.calendar.events.list({
+//               'calendarId': c,
+//               'timeMin': today.toISOString(),
+//               'timeMax': tomorrow.toISOString(),
+//               'showDeleted': false,
+//               'singleEvents': true,
+//               'maxResults': 10,
+//               'orderBy': 'startTime'
+//             }));
+//           });
 
-        /**
-       *  On load, called to load the auth2 library and API client library.
-       */
-      // const handleClientLoad = () => {
-      //   gapi.load('client:auth2', initClient);
-      // }
-useEffect(() => {
-        gapi.load('client:auth2', initClient);
-},[]);
-      /**
-       *  Initializes the API client library and sets up sign-in state
-       *  listeners.
-       */
-      const initClient = () => {
-        gapi.client.init({
-          apiKey: API_KEY,
-          clientId: CLIENT_ID,
-          discoveryDocs: DISCOVERY_DOCS,
-          scope: SCOPES
-        }).then(function () {
-          // Listen for sign-in state changes.
-          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-          // Handle the initial sign-in state.
-          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        }, function(error) {
-          appendPre(JSON.stringify(error, null, 2));
-        });
-      }
-
-      /**
-       *  Called when the signed in status changes, to update the UI
-       *  appropriately. After a sign-in, the API is called.
-       */
-      function updateSigninStatus(isSignedIn) {
-        if (isSignedIn) {
-          setSignedInState(true);
-          listUpcomingEvents();
-        } else {
-          setSignedInState(false);
-        }
-      }
-
-      /**
-       *  Sign in the user upon button click.
-       */
-      function handleAuthClick(event) {
-        gapi.auth2.getAuthInstance().signIn();
-      }
-
-      /**
-       *  Sign out the user upon button click.
-       */
-      function handleSignoutClick(event) {
-        gapi.auth2.getAuthInstance().signOut();
-      }
-
-      /**
-       * Append a pre element to the body containing the given message
-       * as its text node. Used to display the results of the API call.
-       *
-       * @param {string} message Text to be placed in pre element.
-       */
-      function appendPre(message) {
-        var ul = document.getElementById('event-list');
-        var li = document.createElement('li');
-        var textContent = document.createTextNode(message + '\n');
-        li.appendChild(textContent);
-        ul.appendChild(li);
-      }
-
-      function editHeader(date) {
-        var header = document.getElementById('header')
-        header.innerText = 'Events for '.concat(date);
-      }
-
-      /**
-       * Print the summary and start datetime/date of the next ten events in
-       * the authorized user's calendar. If no events are found an
-       * appropriate message is printed.
-       */
-      async function listUpcomingEvents() {
-        var now = new Date();
-        var today = new Date(now.toDateString());
-        var tomorrow = new Date(today.getTime() + 86400000);
-
-        editHeader(now.toDateString().concat('...'));
-        // get all the calendar
-        var calIds = [];
-        var resCalIds = await gapi.client.calendar.calendarList.list();
-        resCalIds.result.items.forEach(function(c) {
-            calIds.push(c.id);
-          });
-
-          var calFetches = [];
-          calIds.forEach(function(c) {
-            calFetches.push(gapi.client.calendar.events.list({
-              'calendarId': c,
-              'timeMin': today.toISOString(),
-              'timeMax': tomorrow.toISOString(),
-              'showDeleted': false,
-              'singleEvents': true,
-              'maxResults': 10,
-              'orderBy': 'startTime'
-            }));
-          });
-
-          var events = [];
-          Promise.allSettled(calFetches).then(function(res) {
-            res.forEach(function(r) {
-              if(r.status === 'fulfilled') {
-                r.value.result.items.forEach(function(i) {
-                  events.push({summary: i.summary, when: i.start.dateTime || null, allDay: !!i.start.date});
-                });
-              }
-            });
-            // all events are now in events[]
-            // sort and display
-           events.sort(function(a,b) { 
-             if(a.allDay) { return -1; } 
-             if(b.allDay) { return 1; } 
-             var dateA = new Date(a);
-             var dateB = new Date(b);
-             if(dateA < dateB) { return -1; }
-             return 1;
-           });
-           editHeader(now.toDateString());
-           events.forEach(function(e) {
-             if(e.allDay || e.when === null) appendPre(e.summary);
-             else appendPre(new Date(e.when).toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'}) + ': ' + e.summary);
-            });
-          });
-      }
-
+//           var events = [];
+//           Promise.allSettled(calFetches).then(function(res) {
+//             res.forEach(function(r) {
+//               if(r.status === 'fulfilled') {
+//                 r.value.result.items.forEach(function(i) {
+//                   events.push({summary: i.summary, when: i.start.dateTime || null, allDay: !!i.start.date});
+//                 });
+//               }
+//             });
+//             // all events are now in events[]
+//             // sort and display
+//            events.sort(function(a,b) { 
+//              if(a.allDay) { return -1; } 
+//              if(b.allDay) { return 1; } 
+//              var dateA = new Date(a);
+//              var dateB = new Date(b);
+//              if(dateA < dateB) { return -1; }
+//              return 1;
+//            });
+//            editHeader(now.toDateString());
+//            events.forEach(function(e) {
+//              if(e.allDay || e.when === null) appendPre(e.summary);
+//              else appendPre(new Date(e.when).toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'}) + ': ' + e.summary);
+//             });
+//           });
+//       }
+console.log(props);
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <script async defer src="https://apis.google.com/js/api.js"></script>
       </Head>
 
       <main className={styles.main}>
-      <p id="header">Events for Today</p>
+      <p id="header">Calendar List</p>
 
 <ul id="event-list">
-
+{props.events?.map(e => (<li key={e.id}>{e.summary}</li>))}
 </ul>
-<button id="authorize_button" style={{display: !signedInState ? 'block' : 'none'}} onClick={() => handleAuthClick()}>Authorize</button>
-<button id="signout_button" style={{display: signedInState ? 'block' : 'none'}} onClick={() => handleSignoutClick()}>Sign Out</button>
-
-<pre id="content"></pre>
 
       </main>
     </div>
@@ -173,10 +91,84 @@ useEffect(() => {
 }
 
 export async function getStaticProps() {
+const auth = await new google.auth.GoogleAuth({
+  keyFile: './calendar-1611181434807-7bf580262a8a.json',
+  scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
+});
+const cal = google.calendar('v3');
+const calList = [
+  'alim.haji@gmail.com', 
+  'dbou2b3k57raho7hcv02ujcs10@group.calendar.google.com', // Dunkin David
+  'addressbook#contacts@group.v.calendar.google.com', // Birthdays
+  'cfsnsbbofrtqfjqdidqj83kgcc@group.calendar.google.com', // Social events
+  '4cu8n1papg1ovklv1944dceo94@group.calendar.google.com' // Dunkin
+];
+const now = new Date();
+const today = new Date(now.toDateString());
+const tomorrow = new Date(today.getTime() + 86400000);
+console.log('today', today);
+console.log('tomorrow', tomorrow);
+const calFetches = [];
+calList.forEach(c => {
+calFetches.push(cal.events.list({
+  auth,
+  calendarId: c,
+  timeMin: today.toISOString(),
+  timeMax: tomorrow.toISOString(),
+  showDeleted: false,
+  // singleEvents: true,
+}));
+});
+const events = [];
+await Promise.allSettled(calFetches).then(res => {
+  res.forEach(r => {
+    if (r.status === 'fulfilled') {
+      r.value.data.items.forEach(i => {
+        events.push({ summary: i.summary, when: i.start.dateTime || null, allDay: !!i.start.date })
+      });
+    }
+  });
+  // events.sort((a,b) => {
+  //            if(a.allDay) { return -1; } 
+  //            if(b.allDay) { return 1; } 
+  //            var dateA = new Date(a);
+  //            var dateB = new Date(b);
+  //            if(dateA < dateB) { return -1; }
+  //            return 1;
+  // });
+});
+console.log('Events', events);
+//           var events = [];
+//           Promise.allSettled(calFetches).then(function(res) {
+//             res.forEach(function(r) {
+//               if(r.status === 'fulfilled') {
+//                 r.value.result.items.forEach(function(i) {
+//                   events.push({summary: i.summary, when: i.start.dateTime || null, allDay: !!i.start.date});
+//                 });
+//               }
+//             });
+//             // all events are now in events[]
+//             // sort and display
+//            events.sort(function(a,b) { 
+//              if(a.allDay) { return -1; } 
+//              if(b.allDay) { return 1; } 
+//              var dateA = new Date(a);
+//              var dateB = new Date(b);
+//              if(dateA < dateB) { return -1; }
+//              return 1;
+//            });
+//            editHeader(now.toDateString());
+//            events.forEach(function(e) {
+//              if(e.allDay || e.when === null) appendPre(e.summary);
+//              else appendPre(new Date(e.when).toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'}) + ': ' + e.summary);
+//             });
+//           });
+//       }
+
 return {
   props: {
-    client_id: process.env.CLIENT_ID,
-    api_key: process.env.API_KEY,
+    header: 'Events for '.concat(today),
+    events,
   }
 };
 }
